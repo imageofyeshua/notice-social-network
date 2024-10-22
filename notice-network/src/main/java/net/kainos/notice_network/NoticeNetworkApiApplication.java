95485b4d6a13c7c567697577fdc553782cb610cf
Daniel Park <intheimageofyeshua@gmail.com>
2024-10-22 09:43:27 +0900

    Initial Setup

---
+++ notice-network/src/main/java/net/kainos/notice_network/NoticeNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package net.kainos.notice_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeNetworkApiApplication.class, args);
+	}
+
+}
---
+++ notice-network/src/test/java/net/kainos/notice_network/NoticeNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kainos.notice_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
