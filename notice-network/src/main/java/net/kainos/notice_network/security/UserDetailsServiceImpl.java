6fc3a435bc7ba13920b60c4886c802b85ddef233
Daniel Park <intheimageofyeshua@gmail.com>
2024-10-23 12:33:17 +0900

    Jwt Implementation

---
+++ notice-network/src/main/java/net/kainos/notice_network/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package net.kainos.notice_network.security;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import net.kainos.notice_network.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
